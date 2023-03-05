86668d0b82d802d0f473e56c9db31eca1bfe162c
Korotetska Anna <avkorotetska@gmail.com>
2023-03-05 21:57:48 +0200

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,14 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import imageMarkup from './markup';
+import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryElements = document.querySelector('.gallery');
 
+galleryElements.innerHTML = imageMarkup(galleryItems);
 
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const formElement = document.querySelector(".feedback-form");
+
+populateForm();
+
+formElement.addEventListener("input", throttle(onFormInput, 500));
+formElement.addEventListener("submit", onFormSubmit);
+
+const formData = {};
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  console.log(formData);
+
+  Object.keys(formData).forEach((key) => delete formData[key]);
+}
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    const refs = {
+      inputEl: formElement.querySelector("input"),
+      textareaEl: formElement.querySelector("textarea"),
+    };
+    const parsedFormData = JSON.parse(savedData);
+    const { email, message } = parsedFormData;
+
+    refs.inputEl.value = email;
+    refs.textareaEl.textContent = message;
+  }
+}
